acbf9b64ac37e959713dac46c04ea1ab6a08ae51
Luke <mackenzieluke94@gmail.com>
2020-05-05 17:00:23 -0400

    minor style change

--- www/src/components/slider/slide.js
+++ www/src/components/slider/slide.js
@@ -15,29 +15,9 @@ const Slide = ({ image, idx, active }) => {
       <Image
         fluid={image.asset.fluid}
         alt={image.alt}
-        sx={{ height: `100%` }}
+        sx={{ height: `100%`, borderRadius: 4 }}
         imgStyle={{ objectFit: `contain` }}
       />
-      {/* {image.caption && (
-        <p
-          sx={{
-            position: `absolute`,
-            bottom: 0,
-            left: `50%`,
-            transform: `translateX(-50%)`,
-            textAlign: `center`,
-            opacity: active ? 1 : 0,
-            background: `rgba(0,0,0,.75)`,
-            color: `white`,
-            px: `8px`,
-            py: `3px`,
-            borderRadius: `5px`,
-            fontSize: [0, 1],
-          }}
-        >
-          {image.caption}
-        </p>
-      )} */}
     </div>
   )
 }
