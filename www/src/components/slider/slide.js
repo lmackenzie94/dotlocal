c6d5d48003a564a1252a7d325f5bd67aa72a7176
Luke <mackenzieluke94@gmail.com>
2020-05-05 16:49:52 -0400

    started adding info button for img captions

--- www/src/components/slider/slide.js
+++ www/src/components/slider/slide.js
@@ -2,6 +2,7 @@
 import React from "react"
 import { jsx } from "theme-ui"
 import Image from "gatsby-image"
+import CaptionCallout from "./caption-callout"
 
 const Slide = ({ image, idx, active }) => {
   return (
@@ -10,6 +11,7 @@ const Slide = ({ image, idx, active }) => {
         flex: `1 0 100%`,
         transform: active ? `translateX(-${idx * 100}%)` : null,
         opacity: active ? 1 : 0,
+        position: `relative`,
       }}
     >
       <Image
@@ -18,26 +20,7 @@ const Slide = ({ image, idx, active }) => {
         sx={{ height: `100%` }}
         imgStyle={{ objectFit: `contain` }}
       />
-      {/* {image.caption && (
-        <p
-          sx={{
-            position: `absolute`,
-            bottom: 0,
-            left: `50%`,
-            transform: `translateX(-50%)`,
-            textAlign: `center`,
-            opacity: active ? 1 : 0,
-            background: `rgba(0,0,0,.75)`,
-            color: `white`,
-            px: `8px`,
-            py: `3px`,
-            borderRadius: `5px`,
-            fontSize: [0, 1],
-          }}
-        >
-          {image.caption}
-        </p>
-      )} */}
+      {image.caption && <CaptionCallout text={image.caption} />}
     </div>
   )
 }
